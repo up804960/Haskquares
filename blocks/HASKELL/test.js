0b2d16d231e8aa29a99816927d0a7b1e1bb153eb
up804960 <36071498+up804960@users.noreply.github.com>
2019-02-13 22:39:38 +0000

    Updated Blocks

--- blocks/HASKELL/test.js
+++ blocks/HASKELL/test.js
@@ -38,7 +38,7 @@ Blockly.Blocks['haskTestInputNum'] = {
             .setCheck(['Null'])
             .appendField("52");
        this.setOutput(true, TypeClass[0]);
-       this.setColour('#7c1c6d');
+       this.setColour('#4286f4');
     }
 };
 
@@ -48,7 +48,7 @@ Blockly.Blocks['haskTestInputReal'] = {
             .setCheck(['Null'])
             .appendField("501");
        this.setOutput(true, TypeClass[1]);
-       this.setColour('#7c1111');
+       this.setColour('#2845a3');
     }
 };
 
@@ -60,4 +60,76 @@ Blockly.Blocks['haskTestInputRealFrac'] = {
        this.setOutput(true, TypeClass[5]);
        this.setColour('#6a1531');
     }
+};
+
+Blockly.Blocks['haskTestCoreEnum'] = {
+  init: function() {
+    this.appendValueInput("inputL")
+        .setCheck(TypeClass[3])
+        .appendField("");
+    this.appendValueInput("inputR")
+        .setCheck(TypeClass[3])
+        .appendField("+");
+    this.setInputsInline(true);
+    this.setOutput(true, TypeClass[3])
+    this.setColour('#7d874e');
+  }
+};
+
+Blockly.Blocks['haskTestInputEnum'] = {
+    init: function() {
+        this.appendValueInput('VALUE')
+            .setCheck(['Null'])
+            .appendField("1047");
+       this.setOutput(true, TypeClass[3]);
+       this.setColour('#7d874e');
+    }
+};
+
+Blockly.Blocks['haskTestInputIntegral'] = {
+    init: function() {
+        this.appendValueInput('VALUE')
+            .setCheck(['Null'])
+            .appendField("404");
+       this.setOutput(true, TypeClass[4]);
+       this.setColour('#7ec962');
+    }
+};
+
+Blockly.Blocks['haskTestCoreIntegral'] = {
+  init: function() {
+    this.appendValueInput("inputL")
+        .setCheck(TypeClass[4])
+        .appendField("");
+    this.appendValueInput("inputR")
+        .setCheck(TypeClass[4])
+        .appendField("+");
+    this.setInputsInline(true);
+    this.setOutput(true, TypeClass[4])
+    this.setColour('#7ec962');
+  }
+};
+
+Blockly.Blocks['haskTestInputRealFloat'] = {
+    init: function() {
+        this.appendValueInput('VALUE')
+            .setCheck(['Null'])
+            .appendField("2.66");
+       this.setOutput(true, TypeClass[7]);
+       this.setColour('#42eef4');
+    }
+};
+
+Blockly.Blocks['haskTestCoreRealFloat'] = {
+  init: function() {
+    this.appendValueInput("inputL")
+        .setCheck(TypeClass[7])
+        .appendField("");
+    this.appendValueInput("inputR")
+        .setCheck(TypeClass[7])
+        .appendField("+");
+    this.setInputsInline(true);
+    this.setOutput(true, TypeClass[7])
+    this.setColour('#42eef4');
+  }
 };
