866024aef472545e1bd4955758c25df371359d39
up804960 <36071498+up804960@users.noreply.github.com>
2019-02-14 14:18:39 +0000

    Reworked block
    blocks now have another variable called "BlockClass" which is used to represent the block's TypeClass in setCheck, output and colour

--- blocks/HASKELL/test.js
+++ blocks/HASKELL/test.js
@@ -5,131 +5,142 @@ goog.provide('Blockly.Blocks.HaskTest');
 
 Blockly.Blocks['haskTestCoreNum'] = {
   init: function() {
+	this.blockClass = TypeClass[0]
     this.appendValueInput("inputL")
-        .setCheck(TypeClass[0])
+        .setCheck(this.blockClass)
         .appendField("");
     this.appendValueInput("inputR")
-        .setCheck(TypeClass[0])
+        .setCheck(this.blockClass)
         .appendField("+");
     this.setInputsInline(true);
-    this.setOutput(true, TypeClass[0])
-    this.setColour('#4286f4');
+    this.setOutput(true, this.blockClass)
+    this.setColour(this.blockClass[4]);
   }
+};
+
+Blockly.Blocks['haskTestInputNum'] = {
+    init: function() {
+		this.blockClass = TypeClass[0]
+        this.appendValueInput('VALUE')
+            .setCheck(['Null'])
+            .appendField("52");
+		this.setOutput(true, this.blockClass);
+		this.setColour(this.blockClass[4]);
+    }
 };
 
 Blockly.Blocks['haskTestCoreReal'] = {
   init: function() {
+	this.blockClass = TypeClass[1]
     this.appendValueInput("inputL")
-        .setCheck(TypeClass[1])
+        .setCheck(this.blockClass)
         .appendField("");
     this.appendValueInput("inputR")
-        .setCheck(TypeClass[1])
+        .setCheck(this.blockClass)
         .appendField("+");
     this.setInputsInline(true);
-    this.setOutput(true, TypeClass[1])
-    this.setColour('#2845a3');
+    this.setOutput(true, this.blockClass)
+    this.setColour(this.blockClass[4]);
   }
 };
 
 
-Blockly.Blocks['haskTestInputNum'] = {
-    init: function() {
-        this.appendValueInput('VALUE')
-            .setCheck(['Null'])
-            .appendField("52");
-       this.setOutput(true, TypeClass[0]);
-       this.setColour('#4286f4');
-    }
-};
-
 Blockly.Blocks['haskTestInputReal'] = {
     init: function() {
+		this.blockClass = TypeClass[1]
         this.appendValueInput('VALUE')
             .setCheck(['Null'])
             .appendField("501");
-       this.setOutput(true, TypeClass[1]);
-       this.setColour('#2845a3');
+		this.setOutput(true, this.blockClass);
+		this.setColour(this.blockClass[4]);
     }
 };
 
 Blockly.Blocks['haskTestInputRealFrac'] = {
     init: function() {
+		this.blockClass = TypeClass[5]
         this.appendValueInput('VALUE')
             .setCheck(['Null'])
             .appendField("255");
-       this.setOutput(true, TypeClass[5]);
-       this.setColour('#6a1531');
+       this.setOutput(true, this.blockClass);
+       this.setColour(this.blockClass[4]);
     }
 };
 
 Blockly.Blocks['haskTestCoreEnum'] = {
   init: function() {
+	this.blockClass = TypeClass[3]
     this.appendValueInput("inputL")
-        .setCheck(TypeClass[3])
+        .setCheck(this.blockClass)
         .appendField("");
     this.appendValueInput("inputR")
-        .setCheck(TypeClass[3])
+        .setCheck(this.blockClass)
         .appendField("+");
     this.setInputsInline(true);
-    this.setOutput(true, TypeClass[3])
-    this.setColour('#7d874e');
+    this.setOutput(true, this.blockClass)
+    this.setColour(this.blockClass[4]);
   }
 };
 
 Blockly.Blocks['haskTestInputEnum'] = {
     init: function() {
+		this.blockClass = TypeClass[3]
         this.appendValueInput('VALUE')
             .setCheck(['Null'])
             .appendField("1047");
-       this.setOutput(true, TypeClass[3]);
-       this.setColour('#7d874e');
+		this.setOutput(true, this.blockClass);
+		this.setColour(this.blockClass[4]);
     }
 };
 
 Blockly.Blocks['haskTestInputIntegral'] = {
     init: function() {
+		this.blockClass = TypeClass[4]
         this.appendValueInput('VALUE')
             .setCheck(['Null'])
             .appendField("404");
-       this.setOutput(true, TypeClass[4]);
-       this.setColour('#7ec962');
+		this.setOutput(true, this.blockClass);
+		this.setColour(this.blockClass[4]);
     }
 };
 
 Blockly.Blocks['haskTestCoreIntegral'] = {
   init: function() {
+	this.blockClass = TypeClass[4]
     this.appendValueInput("inputL")
-        .setCheck(TypeClass[4])
+        .setCheck(this.blockClass)
         .appendField("");
     this.appendValueInput("inputR")
-        .setCheck(TypeClass[4])
+        .setCheck(this.blockClass)
         .appendField("+");
     this.setInputsInline(true);
-    this.setOutput(true, TypeClass[4])
-    this.setColour('#7ec962');
+    this.setOutput(true, this.blockClass)
+    this.setColour(this.blockClass[4]);
   }
 };
 
 Blockly.Blocks['haskTestInputRealFloat'] = {
     init: function() {
+		this.blockClass = TypeClass[7]
         this.appendValueInput('VALUE')
             .setCheck(['Null'])
             .appendField("2.66");
-       this.setOutput(true, TypeClass[7]);
-       this.setColour('#42eef4');
+       this.setOutput(true, this.blockClass);
+       this.setColour(this.blockClass[4]);
     }
 };
 
 Blockly.Blocks['haskTestCoreRealFloat'] = {
   init: function() {
+	this.blockClass = TypeClass[7]
     this.appendValueInput("inputL")
-        .setCheck(TypeClass[7])
+        .setCheck(this.blockClass)
         .appendField("");
     this.appendValueInput("inputR")
-        .setCheck(TypeClass[7])
+        .setCheck(this.blockClass)
         .appendField("+");
     this.setInputsInline(true);
-    this.setOutput(true, TypeClass[7])
-    this.setColour('#42eef4');
+    this.setOutput(true, this.blockClass)
+    this.setColour(this.blockClass[4]);
   }
 };
